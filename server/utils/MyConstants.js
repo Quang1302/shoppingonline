1ec96107d8dac32d7420cfa81bd64e96561b16e7
unknown <Quang1302>
2023-07-14 13:36:11 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clustersgp.a7jhnxb.mongodb.net',
+    DB_USER: 'jokervana14',
+    DB_PASS: 'quang0396916104',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'jokervana14@gmail.com',
+    EMAIL_PASS: 'gcztzvjdstluvfmf',
+    JWT_SECRET: 'jokervana14',
+    JWT_EXPIRES: '31536000000', // in milliseconds
+  };
+  module.exports = MyConstants;
